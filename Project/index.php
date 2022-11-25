846324b44d69c84d3d49e8ae387ac9641835b590
Rezuan Ratul <rezuanislamratul@gmail.com>
2022-11-25 17:47:55 +0600

    MVC Architecture Done

---
+++ Project/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+  </head>
+<body style="background-image: url('index.jpg'); background-size: cover; text-align: center;">
+<h1 style="background-color: rgba(0, 0, 0, 0.75); color: white; font-family: 'Pacifico', cursive; sans-serif; font-size: 75px; text-align: center;">Tiffinbox</h1>
+
+
+<center><h1>
+
+<label style=color:white for="cars">Choose User Type for Login:</label>
+
+<form action="route.php" method="POST">
+
+<select name="route" id="route">
+  <option>Admin</option>
+  <option>Rider</option>
+  <option>Customer </option>
+  <option>Restaurant </option> 
+</select>
+
+<input type="submit" id="cars" name="lname">
+</form>
+
+</h1></center>
+
+<div style="color: white; text-align: center; width: 50vw; padding-left: 25vw">
+<fieldset style="background-color: rgba(0, 0, 0, 0.30)">
+<a href="" style="color: white; text-decoration: none; "><h1> About Us </h1></a>
+            <h4>For us, it's not just abour bringing you good food from your resturants. It's all about making a connection, which is why we sit down with the chefs, dreaming up menus that will arrive fresh and full flavour.</h4> 
+</fieldset>
+</div>
+</br>
+
+<div style="color: white; text-align: center; width: 50vw; padding-left: 25vw">
+<fieldset style="background-color: rgba(0, 0, 0, 0.30)">
+<a href="" style="color: white; text-decoration: none; "><h1> Contact Us </h1></a>
+<h4>It's food lovers like you who inspire us to do what we do. Your comments, ideas and questions push us to go that extra mile, every time. So drop us a line!</h4>
+                <h4><a href="">tiffinbox@contact.com</a></h4></fieldset>
+</div>
+          
+
+
+
+</body>
+</html>
